bce6d7fff7711e5cd42e268b59b717648c239978
Kartik Jolapara <kjmickey003@gmail.com>
2023-02-18 02:29:54 +0530

    sms and email

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,42 @@
-import express from "express";
-import { login } from "../controllers/auth.js";
+import express from 'express';
+import { login } from '../controllers/auth.js';
+import emailUtil from '../utils/email.js';
+import sendSms from '../utils/sendSms.js';
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post('/login', login);
+
+router.post('/email', async (req, res) => {
+  const email = req.body.email;
+  const subject = req.body.subject;
+  const html = req.body.html;
+  const attachments = req.body.attachments;
+
+  const emailUtil1 = await emailUtil.sendEmail({
+    email: 'meetp272003@gmail.com',
+    subject: 'Test',
+    html: 'Test',
+    attachments: []
+  });
+  console.log(email);
+  console.log(subject);
+  console.log(html);
+  console.log(attachments);
+  res.send('Email sent successfully');
+});
+
+router.post('/sms', async (req, res) => {
+  const to = req.body.to;
+  const body = req.body.body;
+
+  const smsUtil = await sendSms({
+    to: to,
+    body: body
+  });
+  console.log(to);
+  console.log(body);
+  res.send('SMS sent successfully');
+});
 
 export default router;
